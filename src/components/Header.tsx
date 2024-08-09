490d618ec15ae46884fe6704d0d42b1cd7344993
Roy Andres <rhavila789@gmail.com>
2024-08-08 19:53:06 -0500

    chore: Update header component with responsive navigation links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,25 @@
 const Header = () => {
   return (
-    <header className="fixed w-full bg-background-200 h-[56px] px-4 py-2 flex items-center place-content-between">
-      <p className="text-primary-100">RoyAndresDev</p>
-      <button>
+    <header className="fixed w-full bg-background-200 h-[56px] px-4 py-2 flex items-center place-content-between lg:place-content-center">
+      <p className="text-primary-100 font-serif lg:absolute left-8">RoyAndresDev</p>
+      <div className="hidden lg:flex gap-3">
+        <a href="#" className="text-primary-100 hover:text-primary-100">
+          Acerca de mí
+        </a>
+        <a href="#" className="text-gray-100 hover:text-primary-100">
+          Proyectos
+        </a>
+        <a href="#" className="text-gray-100 hover:text-primary-100">
+          Experiencia
+        </a>
+        <a href="#" className="text-gray-100 hover:text-primary-100">
+          Habilidades
+        </a>
+        <a href="#" className="text-gray-100 hover:text-primary-100">
+          Contacto
+        </a>
+      </div>
+      <button className="lg:hidden">
         <img src="/menu.svg" alt="Menu icon" />
       </button>
     </header>

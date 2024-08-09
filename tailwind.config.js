7cb0a67c1d40bc45f045ab089b5fe72c1aae5c3b
Roy Andres <rhavila789@gmail.com>
2024-08-08 19:26:39 -0500

    feat: Add custom colors to Tailwind CSS theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      primary: {
+        100: "00FF99",
+        200: "00DF7C",
+        300: "00983D",
+      },
+      complementary: {
+        100: "FF0066",
+      },
+      gray: {
+        100: "DADADA",
+      },
+      background: {
+        100: "333333",
+        200: "434343",
+        300: "5C5C5C",
+      },
+    },
   },
   plugins: [],
 };
